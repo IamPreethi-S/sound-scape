3bc926a5a364b112a31aa71a0505fe0b23582fb7
Preethi Subramanian <preethisubramanian24@gmail.com>
2024-03-10 13:18:21 -0500

    uploaded from local
    uploaded from local

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,48 @@
 // import Image from "next/image";
 
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-   <div className="text-green-500">
-    {/* Hello SoundScape! */}
-    Main content
+  <div
+      className="
+        bg-neutral-900 
+        rounded-lg 
+        h-full 
+        w-full 
+        overflow-hidden 
+        overflow-y-auto
+      "
+    >
+      <Header>
+        <div className="mb-2">
+          <h1 
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            ">
+              Welcome back
+          </h1>
+          <div 
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
+          >
+            <ListItem 
+              name="Liked Songs" 
+              image="/images/liked.png" 
+              href="liked" 
+            />
+          </div>
+        </div>
+      </Header>
    </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box"
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 interface SidebarProps{
     children: React.ReactNode;
 }
@@ -61,10 +62,14 @@ const Sidebar:React.FC<SidebarProps> = ({
                         </div>
                     </Box>
                     <Box className="overflow-y-auto h-full">
-                        Song Library
+                        <Library />
                     </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+            {children}
+             </main>
         </div>
+
     );
 }
 
