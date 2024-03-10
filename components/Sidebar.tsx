d94ba233f625948128ec69604c6d66d9190c52ff
Preethi Subramanian <preethisubramanian24@gmail.com>
2024-03-10 00:26:38 -0600

    uploaded from local
    uploaded from local

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box"
+import SidebarItem from "./SidebarItem";
 interface SidebarProps{
     children: React.ReactNode;
 }
@@ -42,7 +43,25 @@ const Sidebar:React.FC<SidebarProps> = ({
                 "
                 >
                     <Box>
-                        SideBar Navigation
+                        <div
+                            className="
+                            flex
+                            flex-col
+                            gap-y-4
+                            px-5
+                            py-4
+                            ">
+                            {routes.map((item)=>(
+                                    <SidebarItem
+                                    key={item.label}
+                                    {...item}
+                                    />
+                                ))
+                                }
+                        </div>
+                    </Box>
+                    <Box className="overflow-y-auto h-full">
+                        Song Library
                     </Box>
             </div>
         </div>
